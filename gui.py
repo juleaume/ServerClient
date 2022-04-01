90deee997e822dd8969094567058972a31591df5
Guillaume <guillaume.j.gautier@gmail.com>
2022-04-01 11:43:34 +0200

    separated write/read messages

--- gui.py
+++ gui.py
@@ -11,18 +11,14 @@ from server import Server
 from utils import get_available_hosts, Messenger
 
 
-class TextEdit(QTextEdit):
+class TextShow(QTextEdit):
     enter_pressed = pyqtSignal()
 
     def __init__(self, *args, **kwargs):
-        super(TextEdit, self).__init__(*args, **kwargs)
+        super(TextShow, self).__init__(*args, **kwargs)
 
     def keyPressEvent(self, e: QtGui.QKeyEvent) -> None:
-        if e.key() == Qt.Key.Key_Return:
-            self.enter_pressed.emit()
-            e.accept()
-        else:
-            super().keyPressEvent(e)
+        e.ignore()
 
 
 class Window(QMainWindow):
@@ -40,62 +36,60 @@ class Window(QMainWindow):
         self.layout = QHBoxLayout()
         self.central_widget.setLayout(self.layout)
 
-        s_ip, s_port, s_button, s_msg, s_send = self._create_box(
+        s_ip, s_port, s_button, s_msg, s_send, s_messages = self._create_box(
             "Server", QComboBox)
-        c_ip, c_port, c_button, c_msg, c_send = self._create_box("Client")
+        c_ip, c_port, c_button, c_msg, c_send, c_messages = self._create_box(
+            "Client")
 
         s_button.clicked.connect(
             lambda: self.create_server(
-                s_ip.currentText(), int(s_port.text()), s_msg)
+                s_ip.currentText(), int(s_port.text()), s_messages, s_msg)
         )
         c_button.clicked.connect(
-            lambda: self.create_client(c_ip.text(), int(c_port.text()), c_msg))
+            lambda: self.create_client(
+                c_ip.text(), int(c_port.text()), c_messages, c_msg)
+        )
+
+        def send_and_place(endpoint: Messenger, message: QLineEdit,
+                           dialog: QTextEdit):
+            msg = message.text()
+            endpoint.send_message(msg.encode())
+            t = dialog.toPlainText()
+            t = f"{t}>{msg}\n"
+            dialog.setText(t)
+            message.setText('')
 
         s_send.clicked.connect(
-            lambda: self.send_message(self.server, s_msg)
-        )
+            lambda: send_and_place(self.server, s_msg, s_messages))
         c_send.clicked.connect(
-            lambda: self.send_message(self.client, c_msg)
-        )
+            lambda: send_and_place(self.client, c_msg, c_messages))
 
         self.show()
 
-    def send_message(self, endpoint: Messenger, box: QTextEdit):
-        endpoint.send_message(
-            self._get_last_line(box).encode())
-        t = box.toPlainText()
-        t += '\n>'
-        box.setText(t)
-
-    def create_server(self, ip, port, message_box):
+    def create_server(self, ip, port, messages, text_box):
         self.server = Server(ip, port, self.server_message)
         self.server.run()
         self.server_message.connect(
-            lambda: self._update_text(message_box, self.server)
+            lambda: self._update_text(messages, self.server)
         )
-        message_box.setEnabled(True)  # open text box even if no one is
+        text_box.setEnabled(True)  # open text box even if no one is
         # listening, the sending will fail anyway
 
-    def create_client(self, ip, port, message_box):
+    def create_client(self, ip, port, messages, text_box):
         self.client = Client(ip, port, self.client_message)
         self.client_message.connect(
-            lambda: self._update_text(message_box, self.client)
+            lambda: self._update_text(messages, self.client)
         )
         self.client.connect()
         self.client.run()
-        message_box.setEnabled(self.client.connected)  # only open the text
+        text_box.setEnabled(self.client.connected)  # only open the text
         # box if the client is connected to a server
 
     @staticmethod
     def _update_text(box: QTextEdit, endpoint: Messenger):
-        t = box.toPlainText()[:-1]
-        message = f"{t}<{endpoint.message.decode()}\n>"
-        box.setText(message)
-
-    @staticmethod
-    def _get_last_line(box: QTextEdit) -> str:
         t = box.toPlainText()
-        return t.splitlines()[-1][1:]
+        message = f"{t}<{endpoint.message.decode()}\n"
+        box.setText(message)
 
     def _create_box(self, name: str, ip_type=QLineEdit):
         group_box = QGroupBox(name)
@@ -126,27 +120,23 @@ class Window(QMainWindow):
 
         layout.addWidget(address_button)
 
+        message_box = TextShow()
+        layout.addWidget(message_box)
         text_layout = QHBoxLayout()
-        text_box = TextEdit('>')
+        text_box = QLineEdit()
         text_box.setEnabled(False)
         text_layout.addWidget(text_box, 2)
         send_button = QPushButton("↲")
         text_layout.addWidget(send_button, 1, Qt.AlignBottom)
 
-        def click_and_place():
-            send_button.click()
-            cursor = text_box.textCursor()
-            cursor.setPosition(len(text_box.toPlainText()))
-            text_box.setTextCursor(cursor)
-
-        text_box.enter_pressed.connect(click_and_place)
+        text_box.returnPressed.connect(send_button.click)
 
         layout.addLayout(text_layout)
 
         self.layout.addWidget(group_box)
 
         return ip_selector, port_selector, \
-               address_button, text_box, send_button
+               address_button, text_box, send_button, message_box
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         print("closing window")
