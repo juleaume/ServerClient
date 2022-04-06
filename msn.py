3c3b6340864ddb1bd09e32623602a935e3a42544
Guillaume <guillaume.j.gautier@gmail.com>
2022-04-06 11:28:21 +0200

    Added themes

--- msn.py
+++ msn.py
@@ -4,11 +4,20 @@ from typing import Union
 from PyQt5 import QtGui
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QHBoxLayout, \
-    QGroupBox, QVBoxLayout, QLabel, QLineEdit, QPushButton
+    QGroupBox, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox
 
 from client import Client
 from gui import MessageBox
 from utils import log, socket
+
+THEMES = {
+    "Cyberpunk": "background-color: #002b36; "
+                 "color: #b58900;"
+                 "font-size: 15px",
+    "Fallout": "background-color: #001f00; "
+               "color: #1bff80;"
+               "font-size: 15px"
+}
 
 
 class Window(QMainWindow):
@@ -18,11 +27,7 @@ class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setWindowTitle("Messenger")
-        self.setStyleSheet(
-            "background-color: #002b36; "
-            "color: #b58900;"
-            "font-size: 15px"
-        )
+        self.setStyleSheet(THEMES.get("Cyberpunk"))
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout = QHBoxLayout()
@@ -34,6 +39,10 @@ class Window(QMainWindow):
         self._name = socket.gethostname()
         self.client_box.address_button.clicked.connect(self.create_client)
         self.show()
+
+    def _set_stylesheet(self):
+        theme = self.theme_combo.currentText()
+        self.setStyleSheet(THEMES.get(theme))
 
     def create_client(self):
         self.client = Client(
@@ -60,6 +69,13 @@ class Window(QMainWindow):
         layout.addWidget(self.set_username, alignment=Qt.AlignTop)
         self.set_username.clicked.connect(self._set_username)
         self.username_entry.returnPressed.connect(self.set_username.click)
+        theme_label = QLabel("Theme")
+        theme_label.setStyleSheet("text-decoration: underline")
+        layout.addWidget(theme_label, alignment=Qt.AlignTop)
+        self.theme_combo = QComboBox()
+        self.theme_combo.addItems(list(THEMES.keys()))
+        self.theme_combo.currentTextChanged.connect(self._set_stylesheet)
+        layout.addWidget(self.theme_combo, Qt.AlignTop)
 
     def _set_username(self):
         if self.username_entry.text():
